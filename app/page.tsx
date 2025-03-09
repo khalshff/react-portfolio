5d8b82b251ebc8a8752d8f05daabf53a78ad4b63
Khalisah Faroukh <khalshf@gmail.com>
2025-03-09 23:34:32 +0800

    added about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+import About from "./components/About";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import './global.css'
+
 
 export default function Home() {
   return (
     <main className="relative">
       <div>
         <Navbar/>
-        <Header></Header>
+        <Header/>
+        <About/>
       </div>
     </main>
   );
