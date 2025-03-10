5612d70b5373304ebdcb35745633e2f823bfe41c
khalisah.faroukh <khalisah.faroukh@stengg.com>
2025-03-10 16:28:56 +0800

    Added onscroll function for navbar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -19,7 +19,7 @@ const Header = () => {
       <div>
         <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
         </div>
-        <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3'>Hi! I'm Khalisah <Image src={assets.hand_icon} alt='' className='w-6'/></h3>
+        <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3'>Hi! I&apos;m Khalisah <Image src={assets.hand_icon} alt='' className='w-6'/></h3>
         <h1 className='text-3xl sm:text-6xl lg:text-[66px]'>Software Engineer.</h1>
         <p className='max-w-2xl mx-auto'>
         I am a software engineer based in Singapore who is deeply intrigued with the idea of
